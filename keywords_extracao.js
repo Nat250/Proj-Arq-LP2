bee55aae2fb5c30d6e145d8617874d5cf1e39c17
Nat250 <johannesmk@uol.com.br>
2023-05-19 20:31:32 -0300

    19/05

---
+++ keywords_extracao.js
@@ -0,0 +1,20 @@
+// Microsserviço de extração de keywords
+const natural = require('natural');
+natural.PorterStemmer.attach();
+
+// Extraindo keywords
+function extractKeywords(text) {
+  const tokenizer = new natural.WordTokenizer();
+  const words = tokenizer.tokenize(text);
+  const stopwords = natural.stopwords;
+  const filteredWords = words.filter((word) => !stopwords.includes(word.toLowerCase()));
+  const stemmedWords = filteredWords.map((word) => word.stem());
+  const wordFrequency = {};
+  stemmedWords.forEach((word) => {
+    wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+  });
+  const sortedKeywords = Object.keys(wordFrequency).sort((a, b) => wordFrequency[b] - wordFrequency[a]);
+  return sortedKeywords.slice(0, 5);
+}
+
+module.exports = { extractKeywords };
